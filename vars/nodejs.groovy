8d49ae193613ada3a4ae369e80524374e309ed09
Sushma <sushma.gayatri451@gmail.com>
2022-11-13 14:36:49 +0530

    Shared

---
+++ vars/nodejs.groovy
@@ -0,0 +1,31 @@
+def lintCheck() {
+    sh '''
+         # We want Devs to handle the lint checks failure
+         # npm i jslint
+         # node_modules/jslint/bin/jslint.js server.js || true
+         echo starting lint checks
+         echo Lint checks completed
+    '''
+}
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage('Downloading the dependencies') {
+                steps {
+                    sh "npm install"
+                }
+            }
+
+            stage('Lint Checks') {
+                steps {
+                    script {
+                        node.jslintChecks()
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Call is the default function which will be called when you call the filename
---
+++ vars/sample.groovy
@@ -0,0 +1,11 @@
+// def info(message) {
+//     echo "INFO: ${message}"
+// }
+
+// info("Hai") //Calling a function in groovy
+
+def info(message, URL) {            //Declaring a Function in groovy
+    echo "INFO: ${message} , ${URL}"
+}
+
+info("Hai" , "google.com") // Calling a Function in groovy
