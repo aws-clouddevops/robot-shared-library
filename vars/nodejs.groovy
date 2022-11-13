c9b65f75ca7bc612a03d358a570e99c91fdfdb50
Sushma <sushma.gayatri451@gmail.com>
2022-11-13 18:04:34 +0530

    Sonar

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -9,13 +9,16 @@ def lintCheck() {
 }
 def sonarCheck() {
     sh '''
-        sonar-scanner -Dsonar.host.url=http://172.31.7.40:9000 -Dsonar.sources. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password
+        sonar-scanner -Dsonar.host.url=http://172.31.7.40:9000 -Dsonar.sources. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}
 
     '''
 }
 def call() {
     pipeline {
         agent any
+    environment{
+        SONAR = credentials('SONAR')
+    }
         stages {
             stage('Downloading the dependencies') {
                 steps {
