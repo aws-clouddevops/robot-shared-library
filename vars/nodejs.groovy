2c519f3634876134aaf009381d4e78fd1b242108
Sushma <sushma.gayatri451@gmail.com>
2022-11-16 17:32:49 +0530

    commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,11 +1,15 @@
 
 def call() {
     node {
+        git branch: 'main', url: 'https://github.com/aws-clouddevops/${COMPONENT}.git'
         env.APPTYPE='nodejs'
         common.lintCheck()
         env.ARGS="-Dsonar.sources=."
         common.sonarCheck()
         common.testCases()
+        if (env.TAG_NAME != null) {
+            common.artifacts()
+        }
     }
 }
 
