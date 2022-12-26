ea82caa9de11e03d912a91929c773014f153c3b7
Sushma <sushma.gayatri451@gmail.com>
2022-12-26 18:13:09 +0530

    LB

---
+++ vars/terraform.groovy
@@ -0,0 +1,31 @@
+def call() {
+    properties([
+        parameters([
+            choice(name: "ENV" ,choices: 'dev\nprod' , description: "Choose Environment to build")
+            choice(name: "ACTION" ,choices: 'apply\ndestroy' , description: "Choose apply to destroy")
+    ])
+])
+
+    node {
+        sh "rm -rf *"
+        git branch: 'main', url: "https://github.com/aws-clouddevops/${REPONAME}.git"
+
+        stage('Terraform Init'){
+            sh '''
+                terrafile -f env-${ENV}/Terrafile
+                terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars"
+            '''
+        }
+
+        stage('Terraform Plan'){
+            sh '''
+                terraform plan -var-file=env-${ENV}/${ENV}.tfvars
+            '''
+        }
+        stage('Terraform Plan'){
+            sh '''
+                terraform ${ACTION} -var-file=env-${ENV}/${ENV}.tfvars -auto-approve
+            '''
+        }
+    }
+}
